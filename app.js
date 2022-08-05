a205e0b56e5e90164e4de2171b176523f1ca6dc7
Maria Anna Tsapali <100848733+mariannatsapali@users.noreply.github.com>
2022-08-05 14:51:46 +0300

    m

--- app.js
+++ app.js
@@ -171,10 +171,11 @@ app.get("/about", function(req, res) {
 });
 
 
-
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 3000;
+  port = 8000;
 }
-app.listen(port);
-console.log("Server started has started!");
+
+app.listen(port, function() {
+  console.log("Server started has started!");
+});
